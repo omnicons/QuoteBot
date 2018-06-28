6291b722630bbdd04b23972c9a2e50b4ae79f65c
Alex Ramirez <alexir13@gmail.com>
2018-06-27 22:05:19 -0700

    Using switch

--- index.js
+++ index.js
@@ -27,75 +27,154 @@ client.on('message', async message => {
 	if(message.author.bot) return;
 	if(message.content.indexOf(config.prefix) !== 0) return;
 	console.log(message.content);
-	if(command === 'ping') {
-		const pingmessage = await message.channel.send(':stopwatch:');
-		pingmessage.edit(`:stopwatch: Latency [${pingmessage.createdTimestamp - message.createdTimestamp}ms] API Latency [${Math.round(client.ping)}ms]`);
+	switch(command){
+		case 'ping':
+			const pingmessage = await message.channel.send(':stopwatch:');
+			pingmessage.edit(`:stopwatch: Latency [${pingmessage.createdTimestamp - message.createdTimestamp}ms] API Latency [${Math.round(client.ping)}ms]`);
+			break;
+
+		case 'invite':
+			message.channel.send('https://discord.io/quotebot');
+			break;
+
+		case 'h':
+			const embed = new Discord.RichEmbed()
+				.setAuthor(client.user.username, client.user.avatarURL)
+				.setColor(0x00AE86)
+				.setTitle('Available Commands')
+				.addField(`${config.prefix}h`, 'Shows you this!')
+				.addField(`${config.prefix}ping`, 'Gives you the bot\'s response time and API response time.')
+				.addField(`${config.prefix}invite`, 'Provide an invite link to invite this bot to your server! (Not available yet)')
+				.addField(`${config.prefix}purge`, 'Delete between 2 and 100 messages in a channel (Admin Only)')
+				.addField(`${config.prefix}q`, 'Fetch a qoute from the same channel by using `+q <messageid>`(Admin Only)')
+				.setTimestamp()
+				.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
+			message.channel.send({ embed });
+			break;
+
+		case 'q':
+			const originalMessage = message;
+			const channel = message.guild.channels.get(args[1]);
+			if(args[1] !== undefined) {
+				channel.fetchMessage(args[0])
+					.then(message => {
+						const Attachment = (message.attachments).array();
+						let embed = new Discord.RichEmbed()
+							.setColor(0x00AE86)
+							.setTitle(`Quote from #${message.channel.name}:`)
+							.setDescription(message.content)
+							.setAuthor(message.author.username, message.author.avatarURL)
+							.setTimestamp(new Date(message.createdTimestamp).toISOString())
+							.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
+						if (Attachment[0] !== undefined) { embed.setImage(Attachment[0].url); }
+						originalMessage.channel.send({ embed });
+					})
+					.catch(console.error);
+			}
+			else {
+				message.channel.fetchMessage(args[0])
+					.then(message => {
+						const Attachment = (message.attachments).array();
+						let embed = new Discord.RichEmbed()
+							.setColor(0x00AE86)
+							.setTitle(`Quote from #${message.channel.name}:`)
+							.setDescription(message.content)
+							.setAuthor(message.author.username, message.author.avatarURL)
+							.setTimestamp(new Date(message.createdTimestamp).toISOString())
+							.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
+						if (Attachment[0] !== undefined) { embed.setImage(Attachment[0].url); }
+						originalMessage.channel.send({ embed });
+					})
+					.catch(console.error);
+			}
+			break;
+
+		case 'purge':
+			const deleteCount = parseInt(args[0], 10);
+			if(!deleteCount || deleteCount < 2 || deleteCount > 100) {return message.reply(':shield: You can select anywhere from 2 to 100 messages to delete.');}
+			const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+			message.channel.bulkDelete(fetched)
+				.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+			break;
+
+		case 'restart':
+			if (message.author.id === config.admin){
+				process.exit();
+			}
+			break;
+
+		default: break;
 	}
-	if(command === 'invite') {
-		message.channel.send('https://discord.io/quotebot');
-	}
-	if(command === 'h') {
-		const embed = new Discord.RichEmbed()
-			.setAuthor(client.user.username, client.user.avatarURL)
-			.setColor(0x00AE86)
-			.setTitle('Available Commands')
-			.addField(`${config.prefix}h`, 'Shows you this!')
-			.addField(`${config.prefix}ping`, 'Gives you the bot\'s response time and API response time.')
-			.addField(`${config.prefix}invite`, 'Provide an invite link to invite this bot to your server! (Not available yet)')
-			.addField(`${config.prefix}purge`, 'Delete between 2 and 100 messages in a channel (Admin Only)')
-			.addField(`${config.prefix}q`, 'Fetch a qoute from the same channel by using `+q <messageid>`(Admin Only)')
-			.setTimestamp()
-			.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
-		message.channel.send({ embed });
-	}
-	if (command === 'q') {
-		const originalMessage = message;
-		const channel = message.guild.channels.get(args[1]);
-		if(args[1] !== undefined) {
-			channel.fetchMessage(args[0])
-				.then(message => {
-					const Attachment = (message.attachments).array();
-					let embed = new Discord.RichEmbed()
-						.setColor(0x00AE86)
-						.setTitle(`Quote from #${message.channel.name}:`)
-						.setDescription(message.content)
-						.setAuthor(message.author.username, message.author.avatarURL)
-						.setTimestamp(new Date(message.createdTimestamp).toISOString())
-						.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
-					if (Attachment[0] !== undefined) { embed.setImage(Attachment[0].url); }
-					originalMessage.channel.send({ embed });
-				})
-				.catch(console.error);
-		}
-		else {
-			message.channel.fetchMessage(args[0])
-				.then(message => {
-					const Attachment = (message.attachments).array();
-					let embed = new Discord.RichEmbed()
-						.setColor(0x00AE86)
-						.setTitle(`Quote from #${message.channel.name}:`)
-						.setDescription(message.content)
-						.setAuthor(message.author.username, message.author.avatarURL)
-						.setTimestamp(new Date(message.createdTimestamp).toISOString())
-						.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
-					if (Attachment[0] !== undefined) { embed.setImage(Attachment[0].url); }
-					originalMessage.channel.send({ embed });
-				})
-				.catch(console.error);
-		}
-	}
-	if(command === 'purge') {
-		const deleteCount = parseInt(args[0], 10);
-		if(!deleteCount || deleteCount < 2 || deleteCount > 100) {return message.reply(':shield: You can select anywhere from 2 to 100 messages to delete.');}
-		const fetched = await message.channel.fetchMessages({ limit: deleteCount });
-		message.channel.bulkDelete(fetched)
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-	}
-	{if (message.author.id === config.admin) {
-		if (command === 'restart') {
-			process.exit();
-		}
-	}}
+	// if(command === 'ping') {
+	// 	const pingmessage = await message.channel.send(':stopwatch:');
+	// 	pingmessage.edit(`:stopwatch: Latency [${pingmessage.createdTimestamp - message.createdTimestamp}ms] API Latency [${Math.round(client.ping)}ms]`);
+	// }
+	// if(command === 'invite') {
+	// 	message.channel.send('https://discord.io/quotebot');
+	// }
+	// if(command === 'h') {
+	// 	const embed = new Discord.RichEmbed()
+	// 		.setAuthor(client.user.username, client.user.avatarURL)
+	// 		.setColor(0x00AE86)
+	// 		.setTitle('Available Commands')
+	// 		.addField(`${config.prefix}h`, 'Shows you this!')
+	// 		.addField(`${config.prefix}ping`, 'Gives you the bot\'s response time and API response time.')
+	// 		.addField(`${config.prefix}invite`, 'Provide an invite link to invite this bot to your server! (Not available yet)')
+	// 		.addField(`${config.prefix}purge`, 'Delete between 2 and 100 messages in a channel (Admin Only)')
+	// 		.addField(`${config.prefix}q`, 'Fetch a qoute from the same channel by using `+q <messageid>`(Admin Only)')
+	// 		.setTimestamp()
+	// 		.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
+	// 	message.channel.send({ embed });
+	// }
+	// if (command === 'q') {
+	// 	const originalMessage = message;
+	// 	const channel = message.guild.channels.get(args[1]);
+	// 	if(args[1] !== undefined) {
+	// 		channel.fetchMessage(args[0])
+	// 			.then(message => {
+	// 				const Attachment = (message.attachments).array();
+	// 				let embed = new Discord.RichEmbed()
+	// 					.setColor(0x00AE86)
+	// 					.setTitle(`Quote from #${message.channel.name}:`)
+	// 					.setDescription(message.content)
+	// 					.setAuthor(message.author.username, message.author.avatarURL)
+	// 					.setTimestamp(new Date(message.createdTimestamp).toISOString())
+	// 					.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
+	// 				if (Attachment[0] !== undefined) { embed.setImage(Attachment[0].url); }
+	// 				originalMessage.channel.send({ embed });
+	// 			})
+	// 			.catch(console.error);
+	// 	}
+	// 	else {
+	// 		message.channel.fetchMessage(args[0])
+	// 			.then(message => {
+	// 				const Attachment = (message.attachments).array();
+	// 				let embed = new Discord.RichEmbed()
+	// 					.setColor(0x00AE86)
+	// 					.setTitle(`Quote from #${message.channel.name}:`)
+	// 					.setDescription(message.content)
+	// 					.setAuthor(message.author.username, message.author.avatarURL)
+	// 					.setTimestamp(new Date(message.createdTimestamp).toISOString())
+	// 					.setFooter('Bot by Kayda#0001', 'https://cdn.discordapp.com/avatars/81385189875388416/2db9d70f0f9f0d48eb42935e0d25f04d.png?size=2048');
+	// 				if (Attachment[0] !== undefined) { embed.setImage(Attachment[0].url); }
+	// 				originalMessage.channel.send({ embed });
+	// 			})
+	// 			.catch(console.error);
+	// 	}
+	// }
+	// if(command === 'purge') {
+	// 	const deleteCount = parseInt(args[0], 10);
+	// 	if(!deleteCount || deleteCount < 2 || deleteCount > 100) {return message.reply(':shield: You can select anywhere from 2 to 100 messages to delete.');}
+	// 	const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+	// 	message.channel.bulkDelete(fetched)
+	// 		.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+	// }
+	// {if (message.author.id === config.admin) {
+	// 	if (command === 'restart') {
+	// 		process.exit();
+	// 	}
+	// }
+	// }
 });
 
 
